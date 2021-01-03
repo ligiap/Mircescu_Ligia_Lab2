32027f6a9a7c43a917e3ff4b5f0eb22c4f3efa4c
Ligia Mangra <Ligia.Mangra@Yardi.Com>
2021-01-03 17:35:32 +0200

    Am completat Laboratorul 4.

---
+++ StopCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+
+namespace Mircescu_Ligia_Lab2.CustomCommands
+
+{
+    class StopCommand
+    {
+        private static RoutedUICommand launch_command;
+        static StopCommand()
+        {
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+            launch_command = new RoutedUICommand("Launch", "Launch",typeof(StopCommand), myInputGestures);
+        }
+        public static RoutedUICommand Launch
+        {
+            get
+            {
+                return launch_command;
+            }
+        }
+    }
+}
